90652bfa6b5dafdab94679d8a16883c7a174bca9
kofhb <117967016+kofhb@users.noreply.github.com>
2022-11-13 07:01:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -361,7 +361,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f_caption
         if f_caption is None:
             f_caption = f"{files.file_name}"
-        buttons = InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url='https://t.me/rai_info17') ] ] )
+        buttons = InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ UPDATE ⚔️', url='https://t.me/fondness_movi') ] ] )
 
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
